9c13dc917bee3c57ff68bd1bdfc78e904f646f48
ericpastor <ericpastorpadial@gmai.com>
2023-02-13 11:51:21 +0200

    Added New feature learning branch

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,5 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = `77f89f09-3aa6-4de0-98b6-3432c56ac1fd`;
 export const MODAL_CLOSE_SEC = 2.5;
+
+console.log('learning how to use git branch');
